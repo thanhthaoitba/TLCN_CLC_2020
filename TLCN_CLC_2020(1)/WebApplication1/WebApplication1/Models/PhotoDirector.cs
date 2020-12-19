7399e89146090f2d9a6769bd2a7788e8cd22d1aa
thanhthaoitba <thaolth.ba@gmail.com>
2020-12-19 22:46:25 +0700

    delete

---
+++ TLCN_CLC_2020(1)/WebApplication1/WebApplication1/Models/PhotoDirector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PhotoDirector
+    {
+        [Key]
+        public string PhotoProductorId { get; set; }
+        public string Path { get; set; }
+        public string  DirectorId { get; set; }
+        public Directors Directors { get; set; }
+    }
+}
--- src/app/movielist/models/genre.models.ts
+++
@@ -1,9 +0,0 @@
-export enum Genre {
-    All = 'all',
-    Action = 'action',
-    Adventure = 'adventure',
-    Biography = 'biography',
-    Comedy = 'comedy',
-    Drama = 'drama',
-    History = 'history'
-  }
